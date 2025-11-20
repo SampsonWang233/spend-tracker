6ecbf0caf6d9c7ef621cc2f965b5823657040ec0
SampsonWang233 <siyuwang2020@gmail.com>
2025-11-19 21:36:39 -0500

    for netlify

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/spend-tracker/',
+  base: '/', // Change to '/spend-tracker/' if deploying to a subdirectory
   plugins: [react()],
   server: {
     port: 5173,
